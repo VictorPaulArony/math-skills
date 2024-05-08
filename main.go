d7f5f152e5ad916298def0c20b4469e9d103ac41
victorpaularony <smallypauls@gmail.com>
2024-05-08 16:21:23 +0300

    add string converter

--- main.go
+++ main.go
@@ -2,27 +2,43 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+    "fmt"
+	"strings"
+    "io/ioutil"
+    "math"
+    "strconv"
 
-	"math-skills/skills" // Import your local package
+    "math-skills/skills" // Import your local package
 )
 
 func main() {
-	inputStr, err := ioutil.ReadFile("data.txt")
-	if err != nil {
-		fmt.Println("INVALID FILE", err)
-		return
-	}
-	inputFloat64 := skills.StringToInteger(string(inputStr))
-	
-	// Call the correct function from the skills package
-	avarage := skills.Avarage([]float64{inputFloat64})
-	median := skills.Median([]float64{avarage})
-	variance := skills.Variance([]float64{median})
-	std := skills.StandardDeviation([]float64{variance})
-	fmt.Printf("Average: %.2f\n", avarage)
-	fmt.Printf("Median: %.2f\n", median)
-	fmt.Printf("Variance: %.2f\n", variance)
-	fmt.Printf("Standard Deviation: %.2f\n", std)
+    inputStr, err := ioutil.ReadFile("data.txt")
+    if err != nil {
+        fmt.Println("INVALID FILE", err)
+        return
+    }
+
+    numbers:= strings.Split(string(inputStr), "\n")
+	var val []float64
+	for _, num := range numbers{
+		if num == "" {
+            continue
+        }
+    n, err := strconv.ParseFloat(num, 64)
+    if err != nil {
+        fmt.Println("INVALID NUMBERS", err)
+        return
+    }
+	val = append(val, n)
+}
+
+    // Call the correct function from the skills package
+    avg := skills.Avarage(val)
+    fmt.Println("Average:", math.Round(avg))
+    med := skills.Median(val)
+    fmt.Println("Median:", math.Round(med))
+    variance := skills.Variance(val)
+    fmt.Println("Variance:", variance)
+    stdDev := skills.StandardDeviation(val)
+    fmt.Println("Standard Deviation:", stdDev)
 }
--- skills/convert.go
+++ skills/convert.go
@@ -4,8 +4,8 @@ import (
 	"strconv"
 )
 // Function to convert string to float64
-func StringToInteger(s string) float64 {
-	res, err := strconv.ParseFloat(s, 64)
+func StringToInteger(input string) float64 {
+	res, err := strconv.ParseFloat(string(input), 64)
     if err!= nil {
         return 0
     }
